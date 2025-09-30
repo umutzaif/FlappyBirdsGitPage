3459a982b1ca5edf210d15799c684d8400c215f9
UZAI <105314092+umutzaif@users.noreply.github.com>
2025-09-30 12:04:14 +0300

    Add Blazor WASM publish output

---
+++ FlappyBirdsGitPage/build/wwwroot/js/keyboard.js
@@ -0,0 +1,10 @@
+﻿window.flappyKeyboard = {
+    registerSpace: function (dotNetHelper) {
+        document.addEventListener('keydown', function (event) {
+            if (event.code === 'Space') {
+                event.preventDefault();
+                dotNetHelper.invokeMethodAsync('OnSpacePressed');
+            }
+        });
+    }
+};
